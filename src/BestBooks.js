9737dc923883412618413b5413f6dff778ba9087
alaa-adoos <alaaabualadoos@gmail.com>
2022-09-05 19:50:28 +0300

    lab11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,32 +2,32 @@ import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
-
+import './App.css';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: {}
+      books: []
     }
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
-async componentDidMount(){
-let url='http://localhost:3001/books';
-let res=await axios.get(url);
-console.log(res.data);
-try{
-this.setState({
-  books:res.data
+ componentDidMount=()=>{
+  axios
+.get('http://localhost:3001/books')
+.then(result =>{
+  this.setState({
+    books:result.data
+  })
+})
+.catch(err =>{
+  console.log(err);
 })
 }
-catch{
-  //
-}
 
 
- }
+ 
 
   render() {
 
@@ -36,49 +36,42 @@ catch{
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        <Carousel >
         {this.state.books.length ? (
-          
-          <p>Book Carousel coming soon
-
-{ this.state.books.map((item)=>{
-return(
-  <Carousel fade>
-      <Carousel.Item>
-      
-        <Carousel.Caption>
-          <h3>{item.title}</h3>
-          <p>{item.description}</p>
-          <p>{item.status}</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
        
+       
+     
+ this.state.books.map(item=>{
+return(
 
-        <Carousel.Caption>
-          <h3>{item.title}</h3>
-          <p>{item.description}.</p>
-          <p>{item.status}</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        
-
-        <Carousel.Caption>
-        <h3>{item.title}</h3>
+  <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://nuvomagazine.scdn2.secure.raxcdn.com/wp-content/uploads/2013/11/22904721_10154834443420894_5113488636444823980_o.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption >
+        <h3 >{item.title}</h3>
           <p>{item.description}</p>
           <p>{item.status}</p>
+         
         </Carousel.Caption>
       </Carousel.Item>
-    </Carousel>
+      
+       
+          
+  
 )
 })
 
-}
-</p>   
-  ) : (
+
+
+
+
+  ) : ( 
           <h3>No Books Found :(</h3>
         )}
+       </Carousel>
       </>
     )
   }
